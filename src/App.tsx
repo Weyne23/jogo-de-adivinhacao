4e9e20630388c56e74f5e3b795d2cd8a2481082a
Weyne <augustoweyne12@gmail.com>
2025-08-25 21:49:42 -0300

    feature: criado o componente e estilo do meu header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import './App.css'
+import styles from './app.module.css'
+import { Header } from './components/Header'
 
 export function App() {
+  function handleRestartGame() {
+    alert("Reiniciar jogo!");
+  }
 
   return (
-    <div>
-      <h1>HELLO WORD</h1>
+    <div className={styles.container}>
+      <main>
+        {/* "current", "max" e "onRestart" são os parametros do meu componente */}
+        <Header current={5} max={10} onRestart={handleRestartGame}/> 
+      </main>
     </div>
   )
 }
